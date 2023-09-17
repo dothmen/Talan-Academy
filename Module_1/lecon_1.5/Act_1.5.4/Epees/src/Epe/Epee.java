9f67760b6c27d6c82e72be66f6d4eba9eec92633
Dorsaf Othmen <dothmen@dom.tti>
2023-09-17 17:34:22 +0200

    mod

--- Module_1/lecon_1.5/Act_1.5.4/Epees/src/Epe/Epee.java
+++ Module_1/lecon_1.5/Act_1.5.4/Epees/src/Epe/Epee.java
@@ -1,43 +1,38 @@
 package Epe;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Epee {
 
-	public static void main(String[] args) {
-		String[] collegues = {"Alice", "Bob", "Charlie", "David", "Eve"};
+	public static Scanner sc = new Scanner(System.in);
 
-        int N = collegues.length;
-        boolean permute;
-
-        do {
-            permute = false;
-
-            for (int i = 0; i < N - 1; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    int l1 = collegues[i].length();
-                    int l2 = collegues[j].length();
-                    int min = Math.min(l1, l2);
-
-                    for (int k = 0; k < min; k++) {
-                        if (collegues[i].charAt(k) > collegues[j].charAt(k)) {
-                         
-                            String temp = collegues[i];
-                            collegues[i] = collegues[j];
-                            collegues[j] = temp;
-                            permute = true;
-                            break;
-                        }
-                    }
-                }
-            }
-            
-            N--;
-        } while (N > 1 && permute);
-
-        System.out.println("Tableau trié : " + Arrays.toString(collegues));
-       
+    public static void main(String[] args) {
+        String tab[] = new String[14];
+        System.out.println("Entrez les noms de vos collegues:");
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print("Nom de collegue " + (i + 1) + ": ");
+            tab[i] = sc.nextLine();
+        }
+        triBulles(tab);
+    System.out.println("Noms triés : " + Arrays.toString(tab));
     }
 
-	
-	}
-
-
+    public static void triBulles(String[] tableau) {
+        int n = tableau.length;
+        boolean permut;
+        do {
+            permut = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (tableau[i].compareTo(tableau[i + 1]) > 0) {
+                    
+                    String temp = tableau[i];
+                    tableau[i] = tableau[i + 1];
+                    tableau[i + 1] = temp;
+                    permut = true;
+                }
+            }
+            n--;
+        } while (permut);
+    }
+}
