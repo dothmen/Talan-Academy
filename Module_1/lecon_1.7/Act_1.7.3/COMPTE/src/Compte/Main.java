467231507f230a6904f981c79b51ddd80facb538
Dorsaf Othmen <dothmen@dom.tti>
2023-09-19 18:52:59 +0200

    mess

--- Module_1/lecon_1.7/Act_1.7.3/COMPTE/src/Compte/Main.java
+++ Module_1/lecon_1.7/Act_1.7.3/COMPTE/src/Compte/Main.java
@@ -3,11 +3,11 @@ package Compte;
 public class Main {
 
 	public static void main(String[] args) {
-		Account Compte =new Account(500, 0);
-		Paid_Account ComptePayant= new Paid_Account(502, 0, 0);
-		Savings_Account CompteEpargne= new Savings_Account(502, 0, 0.03);
+		Account Compte =new Account(500, 500);
+		Paid_Account ComptePayant= new Paid_Account(502, 600, 0);
+		Savings_Account CompteEpargne= new Savings_Account(502, 600, 0.03);
 		
-		Compte.depost(500);		
+		Compte.depost(200);		
 		System.out.println("Compte simple :");
 		Compte.toconsult();
 		System.out.println("Compte epargne :");
