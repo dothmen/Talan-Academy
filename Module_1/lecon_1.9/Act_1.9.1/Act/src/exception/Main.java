64573964bfb54dbe0125d059a8d4e967326276c6
Dorsaf Othmen <dothmen@dom.tti>
2023-09-21 15:46:25 +0200

    mess

--- Module_1/lecon_1.9/Act_1.9.1/Act/src/exception/Main.java
+++ Module_1/lecon_1.9/Act_1.9.1/Act/src/exception/Main.java
@@ -1,14 +1,31 @@
 package exception;
 
-
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+			try {
+            int userInput = readUserInput();
+            if (userInput < 10 || userInput > 30) {
+                throw new Exception("The value is not in the allowed interval");
+            } else {
+                System.out.println("L'entier saisi est : " + userInput);
+            }
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+
+        }
+    }
+  public static int readUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un nombre entre 10 et 30 inclus : ");
+        int userInput = scanner.nextInt();
+        scanner.close();
+        return userInput;
+    }
     }
 
 
-	}
 
 
