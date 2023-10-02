a2a4bf23ad97bba1d05c2b0fafe63cd4a3bf5892
Dorsaf Othmen <dothmen@dom.tti>
2023-10-02 18:36:15 +0200

    mess

---
+++ Module_2/lecon_2.5/Act-2.5.3/script.js
@@ -0,0 +1,42 @@
+// Créer le contenu de la partie 1
+var part1 = document.createElement("div");
+part1.id = "part1";
+
+var strong = document.createElement("strong");
+strong.textContent = "Talan Academy";
+
+var anchor = document.createElement("a");
+anchor.href = "https://talan.com";
+anchor.textContent = "Talan";
+
+var textNode1 = document.createTextNode(" est une école de reconversion interne à ");
+var textNode2 = document.createTextNode(", qui propose une formation professionnalisante basée sur une pédagogie innovante.");
+
+part1.appendChild(strong);
+part1.appendChild(textNode1);
+part1.appendChild(anchor);
+part1.appendChild(textNode2);
+
+// Créer le contenu de la partie 2
+var part2 = document.createElement("div");
+part2.id = "part2";
+
+var p = document.createElement("p");
+p.textContent = "Les cursus proposés :";
+
+var ul = document.createElement("ul");
+
+var courses = ["Java", "PHP", "Test et validation"];
+for (var i = 0; i < courses.length; i++) {
+    var li = document.createElement("li");
+    li.textContent = courses[i];
+    ul.appendChild(li);
+}
+
+part2.appendChild(p);
+part2.appendChild(ul);
+
+// Ajouter les parties au corps du document
+var body = document.body;
+body.appendChild(part1);
+body.appendChild(part2);
