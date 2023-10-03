0450d3b08a73ba7b19c65fc1e1d0aba9e232750a
Dorsaf Othmen <dothmen@dom.tti>
2023-10-03 20:54:47 +0200

    mess

--- Module_2/lecon_2.6/Act-2.6.2/Scriptévè.js
+++ Module_2/lecon_2.6/Act-2.6.2/Scriptévè.js
@@ -1,27 +1,27 @@
-// Fonction pour charger le fichier XML
+
 function loadXML() {
     var xhttp = new XMLHttpRequest();
+    var url = "http://localhost/wamp/Mon évènement.xml"; 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             parseXML(this);
         }
     };
-    xhttp.open("GET", "Mon évènement.xml", true); // Remplacez "votre_fichier.xml" par le chemin vers votre fichier XML
+    xhttp.open("GET", url, true);
     xhttp.send();
 }
 
-// Fonction pour parser le XML et afficher les noms des événements
+
 function parseXML(xml) {
     var xmlDoc = xml.responseXML;
     var eventNames = xmlDoc.getElementsByTagName("titre");
     var output = "";
 
     for (var i = 0; i < eventNames.length; i++) {
-        output += eventNames[i].childNodes[0].nodeValue + "<br>";
+        output += eventNames[i].textContent + "<br>";
     }
 
     document.getElementById("eventNames").innerHTML = output;
 }
 
-// Appeler la fonction pour charger le XML
 loadXML();
